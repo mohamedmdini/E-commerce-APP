13ff447be7049fc68ff567b387076aeab2a25234
mohamedmdini <mdinimohamed14@gmail.com>
2023-05-13 00:04:02 +0100

    first commit

---
+++ src/common/footer/footer.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>Bonik</h1>
+            <p>The customer is very important, the customer will be followed by the customer. The author freed it and, in pregnancy. Let there be no course in the house. He was also educated as a care provider.</p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>02 Rue Avicienne 2066 Ibn sina MOuruj 002  </li>
+              <li>Email:Mdinimohamed14@gmail.com</li>
+              <li>Phone: +216 96 435 759 </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
